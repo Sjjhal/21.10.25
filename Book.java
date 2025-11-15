70764ee992ca4a2bb5a31ba3c902b795484953b6
Sjjhal <lolk57462@gmail.com>
2025-11-15 22:09:07 +0200

    Add Book class with attributes and methods

---
+++ Book.java
@@ -0,0 +1,36 @@
+package library;
+
+public class Book {
+    protected int id;
+    protected String title;
+    protected String author;
+    protected int year;
+    protected String genre;
+
+    public Book(int id, String title, String author, int year, String genre) {
+        this.id = id;
+        this.title = title;
+        this.author = author;
+        this.year = year;
+        this.genre = genre;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void read() {
+        System.out.println("Читання книги \"" + title + "\".");
+    }
+
+    @Override
+    public String toString() {
+        return "Book{" +
+                "id=" + id +
+                ", title='" + title + '\'' +
+                ", author='" + author + '\'' +
+                ", year=" + year +
+                ", genre='" + genre + '\'' +
+                '}';
+    }
+}
